096c4d13aafe99855c78c1b0d8d221b73d1f4d11
LRamero <lucasramero@gmail.com>
2024-09-17 17:06:07 -0300

    portfolio.py
    Agregado de archivo .py

--- projects/codes/pokemon.py
+++ projects/codes/pokemon.py
@@ -1425,7 +1425,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-##### CvC - JvJ - JvC ####      ###### JvJ_c ##############z
+##### CvC - JvJ - JvC ####      ###### JvJ_c ##############
 ## sys.argv[1] combat_type      ## sys.argv[1] combat_type
 ## sys.argv[2] pk1              ## sys.argv[2] atk_p1
 ## sys.argv[3] pk2              ## sys.argv[3] atk_p2

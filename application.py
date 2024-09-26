c14341a721fd1219fa1fa60d39efab24b112931a
LRam <lucasramero@gmail.com>
2024-09-26 16:58:28 -0300

    Cambios y pruebas para gunicorn

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, send_from_directory, request, Response, jsonify, send_file
-from werkzeug.middleware.proxy_fix import ProxyFix
 import subprocess
 import pandas as pd
 import pickle
@@ -7,7 +6,6 @@ import random as rd
 import numpy as np
 
 app = Flask(__name__, template_folder="", static_folder="./assets")
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 #########################################################
 #              Fuciones para página principal           #
